1bc209aa7dc73f02cdee542588c9f28c064d9fba
Daniel Fuentes <danielfuentes89@users.noreply.github.com>
2020-08-28 21:43:04 -0400

    add RPA Change

---
+++ js/middleware.js
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////////   MIDDLEWARES //////////////////////////////////
+//const _async = (function () {
+    const _async = (() => {
+    
+        //  Funcion Fetch global para request WS
+        //  recibe: url de WS
+        function returnFetch(url,data) {
+            return fetch(url, {
+                method: 'POST',
+                mode: 'cors',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            })
+        }
+    
+        //  funcion asincrona invoca funcion returnFetch (fetch)
+        async function queryFetch(url,data) {
+            const respuesta = await returnFetch(url,data)
+            const json = await respuesta.json()
+    
+            if (respuesta.status !== 200)
+                throw Error(respuesta.statusText);
+    
+            return json;
+        }
+    
+        return {
+            'queryFetch' : queryFetch
+        }
+    })();
+    /////////////////////////////   MIDDLEWARES //////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
