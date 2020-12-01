106f63244b719c09b26edd80ae7eacd402eda2a3
Daniel Fuentes <danielfuentes89@gmail.com>
2020-12-01 18:51:07 -0300

    Clone Repo littleLink para site

--- app.js
+++
@@ -1,4 +0,0 @@
-var fecha = new Date();
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
-document.getElementById('first-btn').innerHTML = fecha.toLocaleDateString("es-ES", options) + ' (hasta 16:00)'
